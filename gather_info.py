cc37a134c76fa4099ffd9f589ffb00afd81f029e
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 23:07:45 -0400

    Update gather_info.py

--- gather_info.py
+++ gather_info.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = '2444bb179390b9dcfadb7f2555682074f885c805'
 
 users = {}
 

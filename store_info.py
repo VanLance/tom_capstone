150f177dd8884dd6c8c87b5fa475033e5c343243
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 23:15:58 -0400

    Create store_info.py

---
+++ store_info.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy impport SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password =db.Column(db.String(50), nullable=False)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

5f81071e0f75fe54d644c32e45f6b2a12f438f0e
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 23:40:22 -0400

    Create complete_info.py

---
+++ complete_info.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy impport SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password =db.Column(db.String(50), nullable=False)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')

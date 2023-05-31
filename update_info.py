ebbb42c4b3143c0ba916d1ad30a607e1be340a44
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 23:23:43 -0400

    Create update_info.py

---
+++ update_info.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy impport SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password =db.Column(db.String(50), nullable=False)
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'username' not in session:
+        return redirect('/login')
+    
+    username = session['username']
+    user = User.query.filter_by(username=username).first()
+
+    if request.method == 'POST':
+        user.email = request.form['email']
+        user.password = request.form['password']
+        db.session.commit()
+        return redirect('/profile')
+    
+    return render_template('profile.html', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)

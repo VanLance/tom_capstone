d62437f067abcd224670dcd5c2460b6c5a020bd3
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-30 23:29:59 -0400

    Update update_info.py

--- update_info.py
+++ update_info.py
@@ -28,5 +28,30 @@ def profile():
     
     return render_template('profile.html', user=user)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if 'username' not in session:
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return render_template('register.html', error='Username already exists')
+
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['username'] = username
+        return redirect('/')
+    
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Provide the user to update their information and a way for users to sign up for the application.

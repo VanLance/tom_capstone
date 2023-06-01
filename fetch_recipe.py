12d0dc4bb713df6f0bc61ef20aac08a2d4fcef4a
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-31 23:37:05 -0400

    Update fetch_recipe.py

--- fetch_recipe.py
+++ fetch_recipe.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, api_key, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
